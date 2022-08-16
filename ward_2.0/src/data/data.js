15bcd5ca3f97f2dc1e94c69f6f950ce5f8da6785
Ihor Hranetskyi <i.hranetskyi@viseven.com>
2022-08-16 22:56:39 +0300

    new words were added

--- ward_2.0/src/data/data.js
+++ ward_2.0/src/data/data.js
@@ -129,7 +129,175 @@ const data = [
     "word": "припускати",
     "translation": "assume",
     "transcription": ""
-  }
+  },
+  {
+    "id": 217,
+    "status": "",
+    "word": "значно",
+    "translation": "considerably",
+    "transcription": "/kənˈsɪd.ɚ.ə.bli/",
+    "example": "He's considerably fatter than he was when I knew him."
+  },
+  {
+    "id": 218,
+    "status": "",
+    "word": "тим не менш",
+    "translation": "nevertheless",
+    "transcription": "/ˌnev.ɚ.ðəˈles/",
+    "example": "I knew a lot about the subject already, but her talk was interesting nevertheless."
+  },
+  {
+    "id": 219,
+    "status": "",
+    "word": "producing a lot of money",
+    "translation": "lucrative",
+    "transcription": "/ˈluː.krə.t̬ɪv/",
+    "example": "The childcare business is not very lucrative."
+  },
+  {
+    "id": 220,
+    "status": "",
+    "word": "залежний",
+    "translation": "addictive",
+    "transcription": "/əˈdɪk.tɪv/",
+    "example": "Tobacco is highly addictive. These nuts are addictive - I can't stop eating them."
+  },
+  {
+    "id": 221,
+    "status": "",
+    "word": "безпорядки",
+    "translation": "riot",
+    "transcription": "/raɪət/",
+    "example": "Riots started in several cities."
+  },
+  {
+    "id": 222,
+    "status": "",
+    "word": "катастрофа",
+    "translation": "disaster",
+    "transcription": "",
+    "example": "It would be a disaster for me if I lost my job."
+  },
+  {
+    "id": 223,
+    "status": "",
+    "word": "обманювати",
+    "translation": "deceive",
+    "transcription": "/dɪˈsiːv/",
+    "example": "The company deceived customers by selling old computers as new ones."
+  },
+  {
+    "id": 224,
+    "status": "",
+    "word": "отримувати",
+    "translation": "obtain",
+    "transcription": "/əbˈteɪn/",
+    "example": "He obtained a law degree from the University of California."
+  },
+  {
+    "id": 225,
+    "status": "",
+    "word": "покидати, залишати",
+    "translation": "abandon",
+    "transcription": "/əˈbændən/",
+    "example": "They were forced to abandon the car."
+  },
+  {
+    "id": 226,
+    "status": "",
+    "word": "заводити кримінальну справу",
+    "translation": "prosecute",
+    "transcription": "",
+    "example": "No one has been prosecuted for the murders."
+  },
+  {
+    "id": 227,
+    "status": "",
+    "word": "приводити до чогось",
+    "translation": "lead to sth",
+    "transcription": "",
+    "example": "A poor diet can lead to health problems in later life."
+  },
+  {
+    "id": 228,
+    "status": "",
+    "word": "неслухняний",
+    "translation": "naughty",
+    "transcription": "",
+    "example": "I'm afraid I borrowed your car without asking. Yes, that was very naughty of you - I needed it at the weekend!"
+  },
+  {
+    "id": 229,
+    "status": "",
+    "word": "прийматися за щось, приступати до чогось",
+    "translation": "get down to sth",
+    "transcription": "",
+    "example": "I must get down to booking the hotels."
+  },
+  {
+    "id": 230,
+    "status": "",
+    "word": "прямий",
+    "translation": "straight",
+    "transcription": "",
+    "example": "She has straight blonde hair."
+  },
+  {
+    "id": 231,
+    "status": "",
+    "word": "негайно",
+    "translation": "straight away",
+    "transcription": "",
+    "example": "Go there straight away."
+  },
+  {
+    "id": 232,
+    "status": "",
+    "word": "виходити",
+    "translation": "get out",
+    "transcription": "",
+    "example": "I'll get out when you stop at the red light."
+  },
+  {
+    "id": 233,
+    "status": "",
+    "word": "надолужувати, бути в курсі",
+    "translation": "catch up",
+    "transcription": "",
+    "example": "Let's meet for a chat - I need to catch up on all the gossip."
+  },
+  {
+    "id": 234,
+    "status": "",
+    "word": "швидкий шлях, коротка дорога",
+    "translation": "shortcut",
+    "transcription": "",
+    "example": "I took a shortcut through the car park."
+  },
+  {
+    "id": 235,
+    "status": "",
+    "word": "додзвонитись",
+    "translation": "get through",
+    "transcription": "",
+    "example": "I called you earlier, but I couldn't get through."
+  },
+  {
+    "id": 236,
+    "status": "",
+    "word": "ломатись",
+    "translation": "break down",
+    "transcription": "",
+    "example": "Our car broke down and we had to push it off the road."
+  },
+  {
+    "id": 237,
+    "status": "",
+    "word": "рухатись, спішити",
+    "translation": "get a move on",
+    "transcription": "",
+    "example": "Come on, get a move on!"
+  },
 ]
 
 export default data;
